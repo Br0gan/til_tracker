bc6692128107f143f9b955b5c343e900806cecaf
Corey Lee <slicklogic@gmail.com>
2014-06-30 20:21:16 -0400

    3rd commit

--- til_tracker.py
+++ til_tracker.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
-import json, urllib.request, time, re
-
-title = []
+import json, urllib.request, time, re, csv
 
 
-def urlcheck(x):
+f = open('/home/corey/Docs/challenge/til_tracker/data/tildata.txt', 'a')
+title = [f]
+print(title)
+def urlcheck(address):
     try:
-        rawdata = urllib.request.urlopen(x).read()
+        rawdata = urllib.request.urlopen(address).read()
     except urllib.error.HTTPError:
         print('Unable to connect......retying')
         time.sleep(10)
-        urlcheck(x)
+        temp = urlcheck(address)
+        return(temp)
     else:
-        print(rawdata)
         return(rawdata)
 
 
@@ -21,22 +22,21 @@ def pullInfo():
    
     url = 'http://www.reddit.com/r/todayilearned/new/.json'
     rawdata = urlcheck(url)
-    print(rawdata)
     data = json.loads(rawdata.decode('utf8'))
     results = data['data']['children']
 
     for eachResult in results:
-        regResult = re.split("[Tt][Ii][Ll][,:\s]", eachResult['data']['title'])         
-        if regResult[1] not in title:
-           title.append(regResult[1].str.capitalize())
-           print(regResult[1],'\n')
-           
-       
-    
+        regResult = re.split("[Tt][Ii][Ll][,:\s-]", eachResult['data']['title'])
+        urlResult = eachResult['data']['url']
+        
+        if (regResult[1], urlResult) not in title:
+           title.append((regResult[1], urlResult))
+           print(regResult[1].capitalize(),'\n',urlResult, '\n')
+           print('****'*10)
+           s = str(regResult[1]) + '\n' + str(urlResult) + '\n \n'
+           f.write(s)
 
 while True:
     pullInfo()
-    print(len(title))
-    print('***'*10)
     time.sleep(60)
 
