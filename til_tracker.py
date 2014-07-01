d97df2b7751b7caa9d83f4efc6f2e21c677213a4
Corey Lee <slicklogic@gmail.com>
2014-06-30 20:34:53 -0400

    3rd commit

--- til_tracker.py
+++ til_tracker.py
@@ -2,13 +2,13 @@
 
 import json, urllib.request, time, re, csv
 
-
+#Open file to write TIL's to,
 f = open('/home/corey/Docs/challenge/til_tracker/data/tildata.txt', 'a')
 
-
+#List to hold recent TIL's
 title = []
 
-
+#a check and retry so to keep program alive.
 def urlcheck(address):
     try:
         rawdata = urllib.request.urlopen(address).read()
@@ -20,7 +20,7 @@ def urlcheck(address):
     else:
         return(rawdata)
 
-
+#Gather most recent TIL's available to scrape the data and append it to the title list to prevent dups.
 def pullInfo():
    
     url = 'http://www.reddit.com/r/todayilearned/new/.json'
@@ -29,13 +29,16 @@ def pullInfo():
     results = data['data']['children']
 
     for eachResult in results:
+        #manuplate the string to remove 'Tils' from the titles.
         regResult = re.split("[Tt][Ii][Ll][,:\s-]", eachResult['data']['title'])
         urlResult = eachResult['data']['url']
-        
+            
         if (regResult[1], urlResult) not in title:
            title.append((regResult[1], urlResult))
            print(regResult[1].capitalize(),'\n',urlResult, '\n')
            print('****'*10)
+          
+          #covernt the tuples to a string to store in tildata.txt
            s = str(regResult[1]) + '\n' + str(urlResult) + '\n \n'
            f.write(s)
 
